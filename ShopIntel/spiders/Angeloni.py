7c7e46bdaaef354f4bdefcd1b8ead304fb4fc95a
Thiago Morilli <thiagomorilli50@gmail.com>
2026-05-12 16:06:31 +0100

    updated

--- ShopIntel/spiders/Angeloni.py
+++ ShopIntel/spiders/Angeloni.py
@@ -1,41 +1,42 @@
 import scrapy
 
+
 class PrecoHunterSpider(scrapy.Spider):
     name = "Angeloni"
-   
-    domains = "https://www.angeloni.com.br/"
-
-    search = "super/"
-
+    domains = "https://www.angeloni.com.br"
+    search = "/super/"
     headers = {
-            
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
-        }
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
+    }
+    custom_settings = {"ROBOTSTXT_OBEY": False}
 
-    
     def start_requests(self):
-
         yield scrapy.Request(
             url=self.domains + self.search,
             method="GET",
             callback=self.category,
-            headers=self.headers,
-            
+            # headers=self.headers,
         )
 
     def category(self, response):
-
-  
-        for categories in response.xpath('//div[@class="superangeloni-main-menu-0-x-mainMenuWrapperCategory"]/h4[@class="flex superangeloni-main-menu-0-x-mainMenuWrapperCategoryContent superangeloni-main-menu-0-x-mainMenuWrapperCategoryContentItems"]/a/@href').getall():
-            
+        for category in response.xpath(
+            '//div[@class="superangeloni-main-menu-0-x-mainMenuWrapper superangeloni-main-menu-0-x-mainMenuWrapperOpen "]/div/div/h4/a/@href'
+        ).getall():
             yield scrapy.Request(
-                    url=self.domains + categories,
-                    method="GET",
-                    callback=self.request_products,
-                    headers=self.headers
-                )
+                url=self.domains + category,
+                method="GET",
+                callback=self.request_products,
+            )
 
     def request_products(self, response):
-                items = response.xpath('//a[@class="vtex-product-summary-2-x-clearLink vtex-product-summary-2-x-clearLink--column-shelf vtex-product-summary-2-x-clearLink--noquick h-100 flex flex-column"]')
+        for products in response.xpath(
+            '//div[@data-af-element="search-result"]/section/a/@href'
+        ).getall():
+            yield scrapy.Request(
+                url=self.domains + products,
+                method="GET",
+                callback=self.product,
+            )
 
-    
+    def product(self, response):
+        print(response)
